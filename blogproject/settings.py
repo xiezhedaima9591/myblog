68172c7d5ab3008058ba407ad8a9946eb9600895
unknown <xiezhedaima9591@gmail.com>
2018-11-09 09:13:46 +0800

    fix picture display

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -124,4 +124,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [os.path.join(STATIC_ROOT, 'blog')]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
