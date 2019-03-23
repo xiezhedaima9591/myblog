065f298c257fb32acdc9f02e08032fb46e93c97f
xiezhedaima9591 <xiezhedaima9591@163.com>
2019-03-23 10:41:47 +0800

    fix bug

---
+++ fabfile.py
@@ -0,0 +1,8 @@
+#-*- coding:utf-8 -*-
+"""
+    filename:       fibfile
+    description:    django learning practice
+    author:         xiezh
+    date:           2018/11/12
+"""
+__author__ = 'xiezh'
